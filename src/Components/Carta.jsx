0eea82ecee57b78f13beaa7dff699e5c618e4170
juanfer <jufer_tar_lch@hotmail.com>
2025-06-19 11:16:51 -0500

    agregado los botones2

--- src/Components/Carta.jsx
+++ src/Components/Carta.jsx
@@ -8,8 +8,8 @@ export const Carta = ({carta}) => {
   return (
    
        
-       <div className='carta-container d-flex flex-column'>
-        <div className="hex-container animate__animated animate__zoomIn" 
+       <div className='carta-container d-flex flex-column animate__animated animate__zoomIn'>
+        <div className="hex-container " 
              style={{backgroundImage: `url(${carta.reverso})`, 
                      backgroundSize: 'cover', 
                      backgroundPosition: 'center'
